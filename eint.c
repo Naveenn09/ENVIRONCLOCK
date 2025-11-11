bc9e58ac821dad2ba563231713b195bfd3ca1f48
Naveenn09 <g.naveenn09@gmail.com>
2025-11-11 12:36:54 +0530

    Fix EINT configuration and interrupt handling

--- eint.c
+++ eint.c
@@ -7,7 +7,7 @@ extern volatile unsigned int menu_mode;
 
 void Init_eint(void)
 {
-    CfgPortPinFunc(0, 3, FUN4);   // P0.1 ? EINT0 (Function 2)
+    CfgPortPinFunc(0, 3, FUN4);   // P0.3 ? EINT1 (Function 4)
 
     EXTMODE |= (1 << 1);
     EXTPOLAR = (1 << 1);
@@ -15,7 +15,7 @@ void Init_eint(void)
    // Clear interrupt flag
     EXTINT = (1 << 1);
     // Enable interrupt in VIC
-    VICIntSelect &= ~(1 << EINT0_VIC_CHNO); // IRQ mode
+    VICIntSelect = 0// IRQ mode
     VICVectAddr0 = (u32)eint0_isr;
     VICVectCntl0 = (1 << 5) | EINT0_VIC_CHNO;
     VICIntEnable |= (1 << EINT0_VIC_CHNO);
@@ -24,6 +24,6 @@ void Init_eint(void)
 void eint0_isr(void) __irq
 {
     menu_mode = 1;             // Set flag for main loop
-    EXTINT = (1 << 1);         // Clear EINT0 flag
+    EXTINT = (1 << 1);         // Clear EINT1 flag
     VICVectAddr = 0;           // Acknowledge VIC
 }
